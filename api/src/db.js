3bbf29622adc9a4369ff37504b322660578269e3
AgustinaCoronado <agustinacoronado26@gmail.com>
2021-03-09 19:22:16 -0300

    agreguerelaciones

--- api/src/db.js
+++ api/src/db.js
@@ -40,6 +40,8 @@ const Home= 'Home';
 
 const compoen ='prueba';
 
+const relaciones = [1,2,3]
+
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
