7d02238d0c5737ea1744ca91b9ca92a60de91ccb
ANDRES <andres2661991@gmail.com>
2021-03-09 18:31:13 -0300

    commit Andres

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,6 +1,8 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+//CAMBIO REALIZADO POR ANDRES
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('product', {
