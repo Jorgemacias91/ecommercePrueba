bf1c8c4bec33927bfeb1e84454434c407377bfb3
AgustinaCoronado <agustinacoronado26@gmail.com>
2021-03-10 11:19:37 -0300

    prueba9

--- api/src/db.js
+++ api/src/db.js
@@ -44,7 +44,7 @@ const relaciones = [1,2,3]
 
 const prueba1= [1,2,3]
 
-const prueba3 = [1,2,3,4, 5]
+const prueba3 = [1,2,3,4, 5,6]
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
